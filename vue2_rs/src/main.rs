14c93f76cc2cc7873e7d9ee5d261e3421c5c6d73
mjarkk <mkopenga@gmail.com>
2022-01-12 16:15:04 +0100

    stuff

--- vue2_rs/src/main.rs
+++ vue2_rs/src/main.rs
@@ -1,3 +1,62 @@
+const input: &'static str = "
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<script>
+module.exports = {}
+</script>
+
+<style lang=\"stylus\" scoped>
+h1
+    color red
+</style>
+";
+
 fn main() {
-    println!("Hello, world!");
+    Parser::parse(input);
+}
+
+struct Parser {
+    source_chars: Vec<char>,
+    source_chars_len: usize,
+    current_char: usize,
+    template: usize,
+    script: usize,
+    styles: Vec<usize>,
+}
+
+impl Parser {
+    fn parse(source: &str) -> Result<Self, String> {
+        let source_chars: Vec<char> = source.chars().collect();
+        let source_chars_len = source_chars.len();
+        let mut p = Self {
+            source_chars,
+            source_chars_len,
+            current_char: 0,
+            template: 0,
+            script: 0,
+            styles: Vec::new(),
+        };
+        p.execute()?;
+        Ok(p)
+    }
+    fn read_byte(&mut self) -> Option<char> {
+        if self.source_chars_len == self.current_char {
+            return None;
+        }
+        let resp = self.source_chars[self.current_char];
+        self.current_char += 1;
+        return Some(resp);
+    }
+    fn execute(&mut self) -> Result<(), String> {
+        while let Some(b) = self.read_byte() {
+            match b {
+                ' ' => {},
+                '<' => {},
+                c => return Err(format!("found invalid character in source: {}, expected <template ..> <script ..> or <style ..>", c.to_string() )),
+            };
+        }
+        Ok(())
+    }
 }
--- vue2_template_compiler/index.js
+++ vue2_template_compiler/index.js
@@ -16,10 +16,71 @@ h1
 </style>
 `
 
-console.log(parse({
+const parsed = parse({
     source,
     filename: 'index.vue',
     compiler,
     sourceRoot: './',
     needMap: true,
-}))
+})
+
+console.log(JSON.stringify(parsed, null, 2))
+
+out = {
+    "template": {
+        "type": "template",
+        "content": "\n<h1>Hello world</h1>\n",
+        "start": 11,
+        "attrs": {},
+        "end": 37
+    },
+    "script": {
+        "type": "script",
+        "content": "//\n//\n//\n//\n//\n\nmodule.exports = {}\n",
+        "start": 58,
+        "attrs": {},
+        "end": 79,
+        "map": {
+            "version": 3,
+            "sources": [
+                "index.vue"
+            ],
+            "names": [],
+            "mappings": ";;;;;;AAMA",
+            "file": "index.vue",
+            "sourceRoot": "./",
+            "sourcesContent": [
+                "\n<template>\n    <h1>Hello world</h1>\n</template>\n\n<script>\nmodule.exports = {}\n</script>\n\n<style lang=\"stylus\" scoped>\nh1\n    color red\n</style>\n"
+            ]
+        }
+    },
+    "styles": [
+        {
+            "type": "style",
+            "content": "\n\n\n\n\n\n\n\n\n\nh1\n    color red\n",
+            "start": 118,
+            "attrs": {
+                "lang": "stylus",
+                "scoped": true
+            },
+            "lang": "stylus",
+            "scoped": true,
+            "end": 136,
+            "map": {
+                "version": 3,
+                "sources": [
+                    "index.vue"
+                ],
+                "names": [],
+                "mappings": ";;;;;;;;;;AAUA;AACA",
+                "file": "index.vue",
+                "sourceRoot": "./",
+                "sourcesContent": [
+                    "\n<template>\n    <h1>Hello world</h1>\n</template>\n\n<script>\nmodule.exports = {}\n</script>\n\n<style lang=\"stylus\" scoped>\nh1\n    color red\n</style>\n"
+                ]
+            }
+        }
+    ],
+    "customBlocks": [],
+    "errors": []
+}
