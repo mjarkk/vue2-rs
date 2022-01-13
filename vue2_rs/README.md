8be97ba8477ca6f98c95766114890840f0816723
mjarkk <mkopenga@gmail.com>
2022-01-13 10:22:28 +0100

    Progress parsing the top level vue file tags

---
+++ vue2_rs/README.md
@@ -0,0 +1,8 @@
+# vue2-rs
+
+A vue 2 template compiler written in rust
+
+## Development links
+
+- html spec https://html.spec.whatwg.org/multipage/syntax.html#syntax-tag-name
+- vue file spec https://vue-loader.vuejs.org/spec.html#intro
--- vue2_rs/src/main.rs
+++ vue2_rs/src/main.rs
@@ -1,4 +1,4 @@
-const input: &'static str = "
+const INPUT: &'static str = "
 <template>
     <h1>Hello world</h1>
 </template>
@@ -14,10 +14,12 @@ h1
 ";
 
 fn main() {
-    if let Err(e) = Parser::parse(input) {
+    if let Err(e) = Parser::parse(INPUT) {
         panic!("{}", e.to_string());
     }
 }
+
+const ERR_EOF: &'static str = "Unexpected EOF";
 
 struct Parser {
     source_chars: Vec<char>,
@@ -43,30 +45,139 @@ impl Parser {
         p.execute()?;
         Ok(p)
     }
-    fn read_byte(&mut self) -> Option<char> {
+    fn seek_one(&mut self) -> Option<char> {
         if self.source_chars_len == self.current_char {
-            return None;
+            None
+        } else {
+            Some(self.source_chars[self.current_char])
         }
-        let resp = self.source_chars[self.current_char];
+    }
+    fn read_one(&mut self) -> Option<char> {
+        let resp = self.seek_one()?;
         self.current_char += 1;
         return Some(resp);
     }
+    fn read_one_skip_spacing(&mut self) -> Option<char> {
+        loop {
+            self.read_one()?;
+
+            match self.read_one()? {
+                ' ' | '\t' | '\n' | '\r' => {}
+                c => return Some(c),
+            };
+        }
+    }
     fn execute(&mut self) -> Result<(), String> {
-        while let Some(b) = self.read_byte() {
+        while let Some(b) = self.read_one_skip_spacing() {
             match b {
-                ' ' | '\t' | '\n' | '\r' => {},
-                '<' => {self.parseTopLevelTag()?;},
+                '<' => {
+                    println!("{:?}", self.parse_top_level_tag()?);
+                },
                 c => return Err(format!("found invalid character in source: '{}', expected <template ..> <script ..> or <style ..>", c.to_string() )),
             };
         }
         Ok(())
     }
-    fn parseTopLevelTag(&mut self) -> Result<TopLevelTag, String> {
-        // TODO
-        Ok(TopLevelTag::Script)
+    fn parse_top_level_tag(&mut self) -> Result<(TopLevelTag, Tag), String> {
+        let parsed_tag = self.parse_tag()?;
+
+        let top_level_tag = match parsed_tag.name(self) {
+            ['t', 'e', 'm', 'p', 'l', 'a', 't', 'e'] => TopLevelTag::Template,
+            ['s', 'c', 'r', 'i', 'p', 't'] => TopLevelTag::Script,
+            ['s', 't', 'y', 'l', 'e'] => TopLevelTag::Style,
+            _ => {
+                return Err(format!(
+                    "unknown top level tag <{}>",
+                    parsed_tag.name_string(self)
+                ))
+            }
+        };
+
+        Ok((top_level_tag, parsed_tag))
+    }
+    // parse_tag is expected to be next to the open indicator (<) at the first character of the tag name
+    // TODO support upper case tag names
+    fn parse_tag(&mut self) -> Result<Tag, String> {
+        let mut tag = Tag {
+            type_: TagType::Open,
+            name_start: self.current_char,
+            args: Vec::new(),
+            name_end: 0,
+        };
+
+        let mut is_close_tag = false;
+        let mut c = self.seek_one().ok_or(ERR_EOF)?;
+        if c == '/' {
+            tag.type_ = TagType::Close;
+            self.current_char += 1;
+            is_close_tag = true;
+        }
+
+        // Parse names
+        loop {
+            c = self.read_one().ok_or(ERR_EOF)?;
+            if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
+                continue;
+            }
+            self.current_char -= 1;
+            tag.name_end = self.current_char;
+            break;
+        }
+
+        c = self.read_one_skip_spacing().ok_or(ERR_EOF)?;
+        match c {
+            '>' => return Ok(tag),
+            '/' => {
+                return if is_close_tag {
+                    Err(String::from("Invalid html tag"))
+                } else {
+                    c = self.read_one_skip_spacing().ok_or(ERR_EOF)?;
+                    if c == '>' {
+                        Ok(tag)
+                    } else {
+                        Err(format!("Expected element closure '>' but got '{}'", c))
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        // Parse args
+        // loop {}
+
+        Ok(tag)
     }
 }
 
+#[derive(Debug)]
+struct Tag {
+    type_: TagType,
+
+    // name_start indicates the tag name start character index in the source
+    name_start: usize,
+    // name_end indicates the tag name end character index in the source
+    name_end: usize,
+
+    args: Vec<(String, String)>,
+}
+
+impl Tag {
+    pub fn name<'a>(&self, parser: &'a Parser) -> &'a [char] {
+        &parser.source_chars[self.name_start..self.name_end]
+    }
+    pub fn name_string(&self, parser: &Parser) -> String {
+        self.name(parser).iter().collect()
+    }
+}
+
+#[derive(Debug)]
+enum TagType {
+    Open,
+    OpenAndClose,
+    Close,
+}
+
+#[derive(Debug)]
 enum TopLevelTag {
     Template,
     Script,
