e76c8bbab7d09a5b9b5bfdb34691a64fcbaa945a
mjarkk <mkopenga@gmail.com>
2022-01-27 10:20:30 +0100

    add docs to code for later

--- src/compiler/template.rs
+++ src/compiler/template.rs
@@ -135,6 +135,79 @@ impl Child {
                 }
                 resp.push('\'');
                 // TODO add args
+                /*
+
+                https://vuejs.org/v2/guide/render-function.html
+                {
+                  // Same API as `v-bind:class`, accepting either
+                  // a string, object, or array of strings and objects.
+                  class: {
+                    foo: true,
+                    bar: false
+                  },
+                  // Same API as `v-bind:style`, accepting either
+                  // a string, object, or array of objects.
+                  style: {
+                    color: 'red',
+                    fontSize: '14px'
+                  },
+                  // Normal HTML attributes
+                  attrs: {
+                    id: 'foo'
+                  },
+                  // Component props
+                  props: {
+                    myProp: 'bar'
+                  },
+                  // DOM properties
+                  domProps: {
+                    innerHTML: 'baz'
+                  },
+                  // Event handlers are nested under `on`, though
+                  // modifiers such as in `v-on:keyup.enter` are not
+                  // supported. You'll have to manually check the
+                  // keyCode in the handler instead.
+                  on: {
+                    click: this.clickHandler
+                  },
+                  // For components only. Allows you to listen to
+                  // native events, rather than events emitted from
+                  // the component using `vm.$emit`.
+                  nativeOn: {
+                    click: this.nativeClickHandler
+                  },
+                  // Custom directives. Note that the `binding`'s
+                  // `oldValue` cannot be set, as Vue keeps track
+                  // of it for you.
+                  directives: [
+                    {
+                      name: 'my-custom-directive',
+                      value: '2',
+                      expression: '1 + 1',
+                      arg: 'foo',
+                      modifiers: {
+                        bar: true
+                      }
+                    }
+                  ],
+                  // Scoped slots in the form of
+                  // { name: props => VNode | Array<VNode> }
+                  scopedSlots: {
+                    default: props => createElement('span', props.text)
+                  },
+                  // The name of the slot, if this component is the
+                  // child of another component
+                  slot: 'name-of-slot',
+                  // Other special top-level properties
+                  key: 'myKey',
+                  ref: 'myRef',
+                  // If you are applying the same ref name to multiple
+                  // elements in the render function. This will make `$refs.myRef` become an
+                  // array
+                  refInFor: true
+                }
+
+                                */
                 resp.push(',');
                 resp.push('[');
                 let children_len = children.len();
