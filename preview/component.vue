eca5d8faedf9ff8d42f5ee1cd87f8e9d8b50390e
mjarkk <mkopenga@gmail.com>
2022-02-21 14:55:53 +0100

    restore changes in preview component

--- preview/component.vue
+++ preview/component.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
     <h1>It wurks {{ count }}!</h1>
-    <div v-if="count">If {{ count }}!</div>
+    <div v-if="count">
+      If <span>{{ count }}</span> !
+    </div>
     <div v-else>Else {{ count }}</div>
     <div v-if="!count">Another else</div>
+    <div>
+      <button v-on:click="count++">+</button>
+      <button @click="count--">-</button>
+    </div>
     <div v-text="'abc'" />
     <div v-html="'abc'" />
     <div v-show="false"><div /></div>
-    <!-- <div foo="bar">
+    <div foo="bar">
       <span v-for="(key, index) in list" :key="key">
         {{ key }} #{{ index }}
       </span>
-    </div> -->
+    </div>
     <input v-model="inputValue" />
-    <button v-on:click="count++">+</button>
-    <button @click="count--">-</button>
+    {{ inputValue }}
   </div>
 </template>
 
